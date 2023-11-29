c75dadf7976ae3afc32050963e1cd93dbe807f01
animerz496981 <148134471+animerz496981@users.noreply.github.com>
2023-11-29 22:01:57 +0530

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -2,13 +2,11 @@ from openai import OpenAI
 import streamlit as st
 
 with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+    openai_api_key = st.text_input("SCMS KEY", key="chatbot_api_key", type="password")
+    
 
-st.title("💬 Chatbot")
-st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
+st.title("SCMSGPT")
+st.caption("SATISH CHANDRA MEMORIAL SCHOOL LLM")
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
@@ -17,7 +15,7 @@ for msg in st.session_state.messages:
 
 if prompt := st.chat_input():
     if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
+        st.info("Please add your scms key to continue.")
         st.stop()
 
     client = OpenAI(api_key=openai_api_key)
